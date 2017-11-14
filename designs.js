a2deb8fe002e6eff08a581106befd735b32a7250
Nechitadi <nechitadi11@gmail.com>
2017-11-14 10:35:50 +0200

    Re-organize code

--- designs.js
+++ designs.js
@@ -1,38 +1,40 @@
 $(document).ready(function() {
-	let color = $('#colorPicker').val();
-
-	$('#colorPicker').on('change', function() {
-		color = $('#colorPicker').val();
-		console.log(color);
-	});
-	
-
 	let gridWidth = 1;
 	let gridHeight = 1;
-
 	let submitSize = $('#submit');
+	let inputHeight = $('#input_height');
+	let inputWidth = $('#input_width');
+	let colorPicker	= $('#colorPicker');
+	let pixelCanvas = $('#pixel_canvas');
+	let color = colorPicker.val();
+
+	colorPicker.on('change', function() {
+		color = colorPicker.val();
+		console.log(color);
+	});
 
 	//Function that assigns the height and width from input fields to variables when the Submit button is clicked.
 	submitSize.on('click', function() {
 		$('tr').remove();
-		gridHeight = $('#input_height').val();
-		gridWidth = $('#input_width').val();
+		gridHeight = inputHeight.val();
+		gridWidth = inputWidth.val();
 		console.log(`height: ${gridHeight}; width: ${gridWidth};`);
 		makeGrid();
 	});	
 
-
+	//Function that creates a table with the height and with that were input
 	function makeGrid() {
 		for(let h=0; h<gridHeight; h++) {
 			let newRow = $('<tr></tr>'); 
-			$('#pixel_canvas').append(newRow);
+			pixelCanvas.append(newRow);
 			for(let w=0; w<gridWidth; w++) {
 				newRow.append('<td></td>');
 			}
 		}
 	}
 
-	$('#pixel_canvas').on('mouseover', 'td', function() {
+	//Function that assigns the color from color picker to the clicked table cell
+	pixelCanvas.on('mouseover', 'td', function() {
 		$('td').click(function(){
 	    $(this).css('background-color', `${color}`);
 	  });
